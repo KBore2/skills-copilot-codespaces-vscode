846ef8e0600875b7b0a50dc33f0d3b0d2433a6ec
KBore2 <99807846+KBore2@users.noreply.github.com>
2024-12-12 14:48:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    // Skills
+    var skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TypeScript",
+        "React",
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "Python",
+        "Java",
+        "C",
+        "C++",
+        "C#",
+        "SQL",
+        "Git"
+    ];
+    return skills;
+}
